7d0f04b9dc61bbb933d28bd3b4c4ec437176e5fa
soulduse <soulduse@naver.com>
2018-01-30 00:44:17 +0900

    [3] PostsRepository 생성

--- src/main/kotlin/com/dave/fish/domain/posts/PostsRepository.kt
+++ src/main/kotlin/com/dave/fish/domain/posts/PostsRepository.kt
@@ -1,7 +1,14 @@
 package com.dave.fish.domain.posts
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.stream.Stream
 
 
 interface PostsRepository: JpaRepository<Posts, Long> {
+
+    @Query("SELECT p " +
+            "FROM Posts p " +
+            "ORDER BY p.id DESC")
+    fun findAllDesc(): Stream<Posts>
 }
