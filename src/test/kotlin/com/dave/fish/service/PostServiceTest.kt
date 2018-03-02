919230052bbf9e336fe7b31d701e1393e05c3fee
soulduse <soulduse@naver.com>
2018-03-02 22:10:09 +0900

    불필요 파일제거

--- src/test/kotlin/com/dave/fish/service/PostServiceTest.kt
+++ src/test/kotlin/com/dave/fish/service/PostServiceTest.kt
@@ -1,15 +1,14 @@
 package com.dave.fish.service
 
 import com.dave.fish.domain.posts.PostsRepository
-import com.dave.fish.dto.posts.PostsMainResponseDto
 import com.dave.fish.dto.posts.PostsSaveRequestDto
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
-import org.assertj.core.api.Assertions.assertThat
 
 
 @RunWith(SpringRunner::class)
