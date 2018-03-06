7c757ab674bb5ca6cb834336224e605c8bf4756e
soulduse <soulduse@naver.com>
2018-03-06 23:51:25 +0900

    DB H2 > Maria (운영디비로 변경)

--- src/main/kotlin/com/dave/fish/Application.kt
+++ src/main/kotlin/com/dave/fish/Application.kt
@@ -13,8 +13,6 @@ val APPLICATION_LOCATIONS = "spring.config.location=" +
         "/app/config/springboot-webservice/real-application.yml"
 
 fun main(args: Array<String>) {
-    println("APPLICATION_LOCATIONS --> $APPLICATION_LOCATIONS")
-
     SpringApplicationBuilder(Application::class.java)
             .properties(APPLICATION_LOCATIONS)
             .run(*args)
